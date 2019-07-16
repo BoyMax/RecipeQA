bf2f43b5b7a8a4f3ab72095f8df9cbe6287cf5ee
李琪琦 <445072942@qq.com>
2019-07-16 11:25:09 +0100

    change dropout to avoid overfitting.

--- TSA_model/train.py
+++ TSA_model/train.py
@@ -49,11 +49,11 @@ def save_model(epoch, model, optimizer,run_loss, val_loss, accuracy, saved_path)
 def log_data(log_path,train_loss,train_accuracy,val_loss,val_accuracy):
     file = open(log_path,'a')
     if torch.cuda.is_available():
-        data = str(train_loss) +' '+ str(f'{train_accuracy:.2f}') \
-            +' '+ str(val_loss)+ ' ' + str(f'{val_accuracy:.2f}')
+        data = str(train_loss) +' '+ str(f'{train_accuracy:.4f}') \
+            +' '+ str(val_loss)+ ' ' + str(f'{val_accuracy:.4f}')
     else:
-        data = str(train_loss) + ' '+ str(f'{train_accuracy:.2f}') \
-                +' '+str(val_loss)+' '+str(f'{val_accuracy:.2f}')
+        data = str(train_loss) + ' '+ str(f'{train_accuracy:.4f}') \
+                +' '+str(val_loss)+' '+str(f'{val_accuracy:.4f}')
     file.write(data)
     file.write('\n')
     file.close()
